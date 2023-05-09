8a5fc69e0060c70f28720be1fb4004002c034c3c
Daniel Sales <danielsalesdesousa@outlook.com>
2023-05-09 17:16:49 -0300

    Game seed

--- dslist/src/main/java/com/example/dslist/entities/Game.java
+++ dslist/src/main/java/com/example/dslist/entities/Game.java
@@ -22,25 +22,31 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
-	private String shrotDescription;
+	
+	@Column (columnDefinition = "TEXT")
+	private String shortDescription;
+	
+	@Column (columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
-			String shrotDescription, String longDescription) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+			String shortDescription, String longDescription) {
 		super();
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
-		this.shrotDescription = shrotDescription;
+		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
 	}
 
@@ -76,12 +82,20 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+    
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
@@ -92,12 +106,12 @@ public class Game {
 		this.imgUrl = imgUrl;
 	}
 
-	public String getShrotDescription() {
-		return shrotDescription;
+	public String getShortDescription() {
+		return shortDescription;
 	}
 
-	public void setShrotDescription(String shrotDescription) {
-		this.shrotDescription = shrotDescription;
+	public void setShortDescription(String shrotDescription) {
+		this.shortDescription = shrotDescription;
 	}
 
 	public String getLongDescription() {
