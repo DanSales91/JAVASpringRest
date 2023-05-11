2239a7a0a878e6576f62d45297c5e2ad1f414b4a
Daniel Sales <danielsalesdesousa@outlook.com>
2023-05-11 10:49:46 -0300

    Deploy Ready

--- dslist/src/main/java/com/example/dslist/services/GameListService.java
+++ dslist/src/main/java/com/example/dslist/services/GameListService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.dslist.dto.GameDTO;
 import com.example.dslist.dto.GameListDTO;
 import com.example.dslist.entities.GameList;
 import com.example.dslist.repositories.GameListRepository;
